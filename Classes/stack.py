724c8860d6c812768e73e711484bfb2f4def3145
Midiros <adamlegner99@gmail.com>
2023-05-14 15:11:40 +0200

    Added class for stack

---
+++ Classes/stack.py
@@ -0,0 +1,28 @@
+class Stack:
+    def __init__(self, items = None):
+        self.__data = items if items else []
+
+    def push(self, item):
+        self.__data.append(item)
+
+    def pop(self):
+        if self.is_empty():
+            print('Stack is empty')
+            return None
+        else:
+            return self.__data.pop()
+
+    def peek(self):
+        if self.is_empty():
+            print('Stack is empty')
+        else:
+            return self.__data[-1]
+
+    def is_empty(self):
+        return len(self.__data) == 0
+
+    def __len__(self):
+        return len(self.__data)
+
+    def __str__(self):
+        return str(self.__data)
