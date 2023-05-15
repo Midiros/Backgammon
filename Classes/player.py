bda3e5963e9dc6e6c3e1af02fd765109f7e89b88
Adam Legner <adamlegner99@gmail.com>
2023-05-15 19:47:31 +0200

    Added function for listing out all the pieces

--- Classes/player.py
+++ Classes/player.py
@@ -2,8 +2,8 @@ from dice import Dice
 from stone import Stone
 
 class Player():
-    player1_positions = [0, 0, 11, 11, 11, 11, 11, 16, 16, 16, 18, 18, 18, 18, 18]
-    player2_positions = [23, 23, 12, 12, 12, 12, 12, 7, 7, 7, 5, 5, 5, 5, 5]
+    # player1_positions = [0, 0, 11, 11, 11, 11, 11, 16, 16, 16, 18, 18, 18, 18, 18]
+    # player2_positions = [23, 23, 12, 12, 12, 12, 12, 7, 7, 7, 5, 5, 5, 5, 5]
 
     def __init__(self, name, player_number):
         while player_number != 1 and player_number != 2:
@@ -15,16 +15,16 @@ class Player():
         self.pieces = []      
         # self.pieces_out = []
         # self.pieces_in = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14] # Na zacatku hry je vsech 15 figurek v poli
-        self.generate_pieces()
+        # self.generate_pieces()
 
-    def generate_pieces(self):
-        for i in range(15):
-            if(self.player_number == 1):
-                # Vytvori 15 figurek/kamenu pro hrace 1
-                self.pieces.append(Stone(self.player_number, i, self.player1_positions[i]))
-            else:
-                # Vytvori 15 figurek/kamenu pro hrace 2
-                self.pieces.append(Stone(self.player_number, i, self.player2_positions[i]))
+    # def generate_pieces(self):
+    #     for i in range(15):
+    #         if(self.player_number == 1):
+    #             # Vytvori 15 figurek/kamenu pro hrace 1
+    #             self.pieces.append(Stone(self.player_number, i, self.player1_positions[i]))
+    #         else:
+    #             # Vytvori 15 figurek/kamenu pro hrace 2
+    #             self.pieces.append(Stone(self.player_number, i, self.player2_positions[i]))
                 
 
     def move_piece(self, piece, move_position):
@@ -36,6 +36,10 @@ class Player():
         self.dice.roll()
         return self.dice.value
     
+    def list_pieces(self):
+        for piece in self.pieces:
+            print(piece)
+    
     def my_score(self):
         return self.score
     
@@ -44,3 +48,4 @@ class Player():
     
     def __str__(self):
         return self.name
+
